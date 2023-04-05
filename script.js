f71d7b0819d8ca101607894c289f5ddc997fba63
Alan-day <lesterthenovice@gmail.com>
2023-04-05 16:45:22 +0100

    attempted filter function

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ import { pokemonArray } from "./data/pokemon.js";
 
 const container = document.querySelector(".card-container");
 
+const input = document.querySelector(".input");
+const button = document.querySelector(".btn");
+
+const displayFilter = (event) => {
+  //didn't work I need more time to figure it out
+  let newArray = pokemonArray.filter((element) => {
+    element.name == input.value;
+  });
+  return newArray;
+};
+
+button.addEventListener("click", displayFilter);
+
 const displayPokemon = (event) => {
   pokemonArray.forEach((element) => {
     container.innerHTML += `<div class = "card">
